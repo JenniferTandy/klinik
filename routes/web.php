b44dba36e3cb00f6337a222ae1bce5721be8f065
JenniferTandy <jennifer.tandy1103@gmail.com>
2024-02-21 16:20:36 +0700

    Update web.php
    saya mengupdate web.php

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('Testing', function () {
-    return view('welcome');
+Route::get('testing', function () {
+    return view('profile');
 });
 
